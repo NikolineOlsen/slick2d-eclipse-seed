5c20bb19bd9c9ae6497675b7d08eacda45e6469c
NikolineOlsen <nikoline.hegelund.olsen@mail.com>
2015-03-11 19:05:37 +0100

    Nyeste version ..

--- src/landerGame/Lander.java
+++ src/landerGame/Lander.java
@@ -22,18 +22,19 @@ public class Lander extends BasicGame {
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		// TODO  is called constantly. This is where all your graphics is done.
+		
 		arg1.drawString("Welcome to the game", 200, 200); //places text at 200, 200 in the screen
 		arg1.setColor(Color.red);
 		
 		//after loading the spaceship image in "init", we draw it in "render"
-		arg1.drawImage(hero, 100, 100); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
+		arg1.drawImage(hero, 20, 20); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		// TODO  is called when the game starts. You can put code here to set things up for your game, such as loading resources like images and sounds.
 		//loading the spaceship image:
-		hero = new Image("Spaceship.png");
+		hero = new Image("minispace.png");
 	} 	
 
 	@Override
@@ -48,6 +49,7 @@ public class Lander extends BasicGame {
 		Lander game = new Lander("Lander Game");
 		try {
 			AppGameContainer container = new AppGameContainer(game);
+			container.setDisplayMode(800, 600, false); //size for the game window
 			container.start();
 		} catch (SlickException e) {
 			// TODO Auto-generated catch block
