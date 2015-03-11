5e5e8c674b6010a6070dfa9534529c2dafef0486
NikolineOlsen <nikoline.hegelund.olsen@mail.com>
2015-03-11 17:11:49 +0100

    Har addet en rumraket til selve baggrunden og det hele, da det er dér koden til spriten skal være. (ellers kan den ikke blive vist). Kan godt være i ikke kan se rumrakketten og det er muligvis fordi den ikke er gemt på jeres computer...

--- src/landerGame/Lander.java
+++ src/landerGame/Lander.java
@@ -5,11 +5,14 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 //class for general game folder, uses Slick2d library "Basic Game"
 //is built from this tutorial:
 // https:baksteenbrick.wordpress.com/2011/02/08/writing-java-games-with-slick-part-two-hello-world/
 public class Lander extends BasicGame {
+	
+	Image hero; //this is where we initialize the image of the spaceship
 
 	public Lander(String title) {
 		super(title);
@@ -21,12 +24,16 @@ public class Lander extends BasicGame {
 		// TODO  is called constantly. This is where all your graphics is done.
 		arg1.drawString("Welcome to the game", 200, 200); //places text at 200, 200 in the screen
 		arg1.setColor(Color.red);
+		
+		//after loading the spaceship image in "init", we draw it in "render"
+		arg1.drawImage(hero, 100, 100); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		// TODO  is called when the game starts. You can put code here to set things up for your game, such as loading resources like images and sounds.
-
+		//loading the spaceship image:
+		hero = new Image("Spaceship.png");
 	} 	
 
 	@Override
--- src/landerGame/Spaceship.java
+++ src/landerGame/Spaceship.java
@@ -3,10 +3,15 @@ package landerGame;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Spaceship extends BasicGame {  //this is the class for moving the spaceship. BasicGame contains all the basics to create a simple game
 
+	//this is the place where we create all the variables
+	//the game images:
+	Image hero;
+	
 	public Spaceship(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
@@ -15,12 +20,17 @@ public class Spaceship extends BasicGame {  //this is the class for moving the s
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException { //render is called constantly. This is where all graphics is done.
 		// TODO Auto-generated method stub
+		//after loading the spaceship image in "init", we draw it in "render"
+		arg1.drawImage(hero, 100, 100); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
 
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException { //init is called when the game starts. This is where we set things up for the game, like load resources like images and sound.
 		// TODO Auto-generated method stub
+		//loading the spaceship image:
+		hero = new Image("Spaceship.png");
+	
 
 	}
 
