f16d57b0182ba0942597c954b80dacc4d705856d
NikolineOlsen <nikoline.hegelund.olsen@mail.com>
2015-03-11 14:41:23 +0100

    platform class

--- src/mainProjectFolder/PlatformClass.java
+++ src/mainProjectFolder/PlatformClass.java
@@ -1,6 +1,32 @@
 package mainProjectFolder;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+import example.SimpleSlickGame;
+
 //this class will be used to create platforms for the game
 //which will be placeholders for what will become planets
 public class PlatformClass {
+	
+	public static void main(String[] args) {
+		
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
+			appgc.setDisplayMode(840, 640, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+		
+	}
 
-}//hello
+}
