d8ec47b5723671bfb59ba04928f276f9166a4de5
NikolineOlsen <nikoline.hegelund.olsen@mail.com>
2015-03-11 15:57:10 +0100

    Et nyt spaceship class... Her skal der defineres hvordan det skal bevæge sig

---
+++ src/landerGame/Spaceship.java
@@ -0,0 +1,38 @@
+package landerGame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Spaceship extends BasicGame {  //this is the class for moving the spaceship. BasicGame contains all the basics to create a simple game
+
+	public Spaceship(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException { //render is called constantly. This is where all graphics is done.
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException { //init is called when the game starts. This is where we set things up for the game, like load resources like images and sound.
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException { //update is called periodically (usually every 20 milliseconds), this is where the game logic is done
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) { //everything starts in main
+		// TODO Auto-generated method stub
+
+	}
+
+}
