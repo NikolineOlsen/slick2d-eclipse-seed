23c91bc2a2726da5b6f3ec8b6ed37e9bd3c19188
Alryvo <Alexryvo@yahoo.dk>
2015-03-13 13:30:36 +0100

    here is the planet class. in order to use it you must define a global Image variable in "lander" and then call the planet.render in the render function of "lander"

---
+++ src/landerGame/planet.java
@@ -0,0 +1,55 @@
+package landerGame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+
+public class planet {
+
+	
+	
+	int gravity;
+	
+	int fuelamount;
+	
+	Image planetimage;
+	
+	Vector2f position;
+	
+	public planet(int size,int xlocation, int ylocation){
+		
+		position = new Vector2f(xlocation, ylocation);
+		
+		if(size == 1){
+			gravity = 100;        //<--- numbers for gravity and fuel are temporary. they will likely be changed once we implement fuel and gravity
+			fuelamount = 100;
+			try {
+				planetimage = new Image("landerGame/resources/tempsmallplanet.jpg");
+			} catch (SlickException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		if(size == 2){
+			gravity = 200;
+			fuelamount = 300;
+		}
+		if(size == 3){
+			gravity = 200;
+			fuelamount = 300;
+			
+		}
+	}
+		public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+			arg1.drawImage(planetimage,position.x,position.y);
+	}
+	
+	
+	
+	
+	
+}
