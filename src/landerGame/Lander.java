fff686a50664722d9f38929288b0c33452705c98
Alexander Skytte Jensen <deviantbora@gmail.com>
2015-03-13 13:14:35 +0100

    Have moved all spaceship code into the Spaceship class and cleaned up comments.

--- src/landerGame/Lander.java
+++ src/landerGame/Lander.java
@@ -6,19 +6,16 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
+//import org.newdawn.slick.Input; //is for handling key input
 import org.newdawn.slick.SlickException;
 //class for general game folder, uses Slick2d library "Basic Game"
 //is built from this tutorial:
 // https:baksteenbrick.wordpress.com/2011/02/08/writing-java-games-with-slick-part-two-hello-world/
 public class Lander extends BasicGame {
 	
-	Image hero; //this is where we initialize the image of the spaceship
+	//Image hero; 
 	Image bg;
-	float x = 20.0f; //x-coordinates for spaceship
-	float y = 5.0f; // y-coordinates for spaceship
-	float speed = 0.2f; //speed of spaceship
-
+	Spaceship player = new Spaceship("Player"); //creates new spaceship object. It's methods is called later in this class
 	
 	public Lander(String title) {
 		super(title);
@@ -29,71 +26,33 @@ public class Lander extends BasicGame {
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		// TODO  is called constantly. This is where all your graphics is done.
 		arg1.drawImage(bg,0,0);
-		//after loading the spaceship image in "init", we draw it in "render"
-		arg1.drawImage(hero, x, y); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
-		
 		arg1.drawString("Welcome to the game", 200, 200); //places text at 200, 200 in the screen
-
 		//arg1.setColor(Color.red);
 		arg1.setColor(new Color(0,255,150)); // A  yucky green defined using three integers
 		//arg1.setColor(new Color(1.0f,0.5f,0.5f,0.8f)); // A light purple with 80% transparency defined using four floats
-		arg1.drawRect(180, 170, 210, 80);
-
-		arg1.setColor(Color.red);
+		arg1.drawRect(180, 170, 210, 80);	
 		
+		player.render(arg0, arg1); // calls render method in Spaceship class
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		// TODO  is called when the game starts. You can put code here to set things up for your game, such as loading resources like images and sounds.
-		//loading the spaceship image:
-		hero = new Image("landerGame/Resources/minispace.png");
 		bg = new Image("landerGame/resources/spaceBg.jpg");
-		
+		player.init(arg0); //calls init method in Spaceship
 	} 	
 
 	@Override
 	public void update(GameContainer arg0, int delta) throws SlickException {
-		// TODO  is called periodically, usually every 20 milliseconds, but the time will vary depending on how much processing you’ll be putting in. This is where the game logic is done.
-
-		Input input = arg0.getInput(); //asks Slick2D what keys are being pressed
-		
-		//movements of the spaceship
-		//if the right arrow is being pressed:
-		if(input.isKeyDown(Input.KEY_RIGHT))
-		{
-			x+= speed * delta; //going in the positive x-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
-		}
-
-		//if the left arrow is being pressed:
-		if(input.isKeyDown(Input.KEY_LEFT))
-		{
-			x-= speed * delta; //going in the decreasing x-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
-		}
-		
-		//if the down arrow is being pressed:
-		if(input.isKeyDown(Input.KEY_DOWN))
-		{
-			y+= speed * delta; //going in the positive y-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
-		}
-		
-		//if the up arrow is being pressed:
-		if(input.isKeyDown(Input.KEY_UP))
-		{
-			y-= speed * delta; //going in the decreasing y-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
-		}
-		
-		
+		player.update(arg0, delta); // calls Spaceship class update method, where movement is coded
 	}
 
 	public static void main(String[] args) {
-		// TODO  Everything starts here. Let’s put in the first few lines of code to get things going
-
 		Lander game = new Lander("Lander Game");
 		try {
 			AppGameContainer container = new AppGameContainer(game);
 			container.setDisplayMode(800, 600, false); //size for the game window
-			container.start();
+			container.start();	
 		} catch (SlickException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/landerGame/Spaceship.java
+++ src/landerGame/Spaceship.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Spaceship extends BasicGame {  //this is the class for moving the spaceship. BasicGame contains all the basics to create a simple game
@@ -11,38 +12,67 @@ public class Spaceship extends BasicGame {  //this is the class for moving the s
 	//this is the place where we create all the variables
 	//the game images:
 	Image hero;
+	float x = 20.0f; //x-coordinates for spaceship
+	float y = 5.0f; // y-coordinates for spaceship
+	float speed = 0.2f; //speed of spaceship
 	
 	public Spaceship(String title) { // remember to call this in Landers Main
 		super(title);
 		// TODO Auto-generated constructor stub
+	
 	}
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException { //render is called constantly. This is where all graphics is done.
 		// TODO Auto-generated method stub
 		//after loading the spaceship image in "init", we draw it in "render"
-		arg1.drawImage(hero, 100, 100); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
-
+		arg1.drawImage(hero, x, y); //it's called arg1, after "Graphics arg1", above. The spaceship is drawn at location (100,100)
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException { //init is called when the game starts. This is where we set things up for the game, like load resources like images and sound.
 		// TODO Auto-generated method stub
 		//loading the spaceship image:
-		hero = new Image("Spaceship.png");
-	
+		hero = new Image("landerGame/Resources/minispace.png");
 
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException { //update is called periodically (usually every 20 milliseconds), this is where the game logic is done
-		// TODO Auto-generated method stub
+	public void update(GameContainer arg0, int delta) throws SlickException {
+		// TODO  is called periodically, usually every 20 milliseconds, but the time will vary depending on how much processing you’ll be putting in. This is where the game logic is done.
 
+		Input input = arg0.getInput(); //asks Slick2D what keys are being pressed
+		
+		//movements of the spaceship
+		//if the right arrow is being pressed:
+		if(input.isKeyDown(Input.KEY_RIGHT))
+		{
+			x+= speed * delta; //going in the positive x-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
+		}
+
+		//if the left arrow is being pressed:
+		if(input.isKeyDown(Input.KEY_LEFT))
+		{
+			x-= speed * delta; //going in the decreasing x-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
+		}
+		
+		//if the down arrow is being pressed:
+		if(input.isKeyDown(Input.KEY_DOWN))
+		{
+			y+= speed * delta; //going in the positive y-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
+		}
+		
+		//if the up arrow is being pressed:
+		if(input.isKeyDown(Input.KEY_UP))
+		{
+			y-= speed * delta; //going in the decreasing y-direction. multiplied by delta so the FPS is stabilized for all computers(same speed for all).
+		}
+		
 	}
-
+	
 	public static void main(String[] args) { //everything starts in main
 		// TODO Auto-generated method stub
-
+	
 	}
 
 }
