29bdae68796467730f27bd76502c637a5df5098e
Alryvo <Alexryvo@yahoo.dk>
2015-05-08 16:51:12 +0200

    spaceship commented

--- src/landerGame/Spaceship.java
+++ src/landerGame/Spaceship.java
@@ -27,31 +27,31 @@ public class Spaceship extends BasicGame {
 
 	float speed = 0.2f; // speed of spaceship
 	float width; // width of player
-	float height; // height of player'
+	float height; // height of player
 	public float health = 100; // health of player
 	int score = 0;
 
 	AffineTransform transformer = new AffineTransform(); // initializing the AffineTransform method, that will help rotate the spaceship
 
 
-	public double accelx = 0;
+	public double accelx = 0;//the acceleration of the spaceship for x and y. these will be added to the x and y
 	public double accely = 0;
-	public float angle = -90;
+	public float angle = -90;//the angle of the spaceship
 	public double throttlex;// throttlex and -y will control the difference in
 							// acceleration when maneuvering the ship
 	public double throttley;
-	public boolean ignition = false;
+	public boolean ignition = false;//this will be used to check if the ignition sprite should be showed
 
 	public int fuelTank = 1000; // starting value of fuel tank of spaceship
 	public int fuelTankLow = 0;// new value that switches place with fuelTank
 								// when fuelTank = 0
-	public int platformFuel = 1000;
+	public int platformFuel = 1000; //this is the fuel amount for the various platforms. these should ideally be loacated in the platform-class
 	public int platformFuel2 = 1000;
 	public int platformFuel3 = 1000;
-	public boolean shipTooFast = false;
+	public boolean shipTooFast = false;//this was supposed to be used to check if the landing ship was landing to fast but somehow that doesn't work
 
 	
-	public planet closestPlanet = null;
+
 
 	public Spaceship(String title) { // remember to call this in Landers Main
 		super(title);
@@ -89,6 +89,8 @@ public class Spaceship extends BasicGame {
 		Input input = arg0.getInput(); // asks Slick2D what keys are being pressed
 
 		movement();
+		
+		//below the if-statements are checking what arrow keys are being pressed
 
 		if (input.isKeyDown(Input.KEY_RIGHT) || input.isKeyDown(Input.KEY_D)) {
 			rotateshipright();
@@ -99,7 +101,7 @@ public class Spaceship extends BasicGame {
 			rotateshipleft();
 
 		}
-		throttlex = 0.001f * Math.cos(Math.toRadians(angle)) * delta;
+		throttlex = 0.001f * Math.cos(Math.toRadians(angle)) * delta;//here we use the sine and cosine to determine the angle
 		throttley = 0.001f * Math.sin(Math.toRadians(angle)) * delta;
 
 		// if the up arrow is being pressed:
@@ -131,8 +133,7 @@ public class Spaceship extends BasicGame {
 		
 		}
 
-		// if ships angle is off it loses life, it the angle is right it gets
-		// fuel, when landing on platform
+		// if ships angle is off it loses life, it the angle is right it gets fuel, when landing on platform
 		if (CollisionDetection.collidesWithPlatform == true) {
 			shipCollision();
 			// only gives as much fuel there is in the platform
@@ -189,15 +190,17 @@ public class Spaceship extends BasicGame {
 						
 	}
 
+	/**
+	 * this stops the ship by canceling out the movement values with their negative values
+	 */
 	private void shipCollision() {
 
-		x += accelx - Lander.testplanet.gx; // this stops the ship by canceling out the movement values with their negative values
+		x += accelx - Lander.testplanet.gx;
 		y += accely - Lander.testplanet.gy;
 	
 		stopShip();
 		
-		if ((accelx + accely) / 2 > -0.5) { // if ship collides and goes too
-											// fast, health is reduced
+		if ((accelx + accely) / 2 > -0.5) { // if ship collides and goes too fast, health is supposed to be reduced. this however doesn't currently work
 			shipTooFast = true;
 			//health -= 0.2f;
 			
@@ -215,7 +218,9 @@ public class Spaceship extends BasicGame {
 			System.out.println("Angle is right");
 		}
 	}
-
+/**
+ * here we change the x and y values by adding the acceleration variables. We check if the player is within any of the gravitational zones in which case the gx and gy values are added
+ */
 	public void movement() { // This function moves the ship in it's current
 								// direction
 		if (CollisionDetection.GravitationalEffect == true){
@@ -238,20 +243,27 @@ public class Spaceship extends BasicGame {
 		accely = accely / 1.001;
 	}
 
+	/**
+	 * this rotates the spaceship angle of movement and image left 
+	 */
 	public void rotateshipleft() {
-		angle -= 1;
-		player.rotate(-1);
+		angle -= 1;//this changes the angle of movement
+		player.rotate(-1);//this rotates the image
 		ignitionSprite.rotate(-1);
 
 	}
-
+	/**
+	 * this rotates the spaceship angle of movement and image right 
+	 */
 	public void rotateshipright() {
-		angle += 1;
-		player.rotate(1);
+		angle += 1;//this changes the angle of movement
+		player.rotate(1);//this rotates the image
 		ignitionSprite.rotate(1);
 
 	}
-	
+	/**
+	 * Throttling adds to the acceleration and moves the ship forward
+	 */
 	public void throttling() {
 		// here the throttle value (with it's embedded angular value) is added
 		// to the acceleration
@@ -263,6 +275,9 @@ public class Spaceship extends BasicGame {
 		accely += throttley;
 		}
 	}
+	/**
+	 * this puts the acceleration to 0
+	 */
 
 	public void stopShip() { // method for stopping ship
 		accelx = 0;
